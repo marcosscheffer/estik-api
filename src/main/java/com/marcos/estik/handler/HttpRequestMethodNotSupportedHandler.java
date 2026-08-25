4302d76cb5beacfef5ae13032fb2751e2ce80376
marcosscheffer <marcosscheffer2989@gmail.com>
2026-08-25 13:06:24 -0300

    feat: more handlers

---
+++ src/main/java/com/marcos/estik/handler/HttpRequestMethodNotSupportedHandler.java
@@ -0,0 +1,21 @@
+package com.marcos.estik.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.marcos.estik.domain.dto.ApiErrorDTO;
+
+@RestControllerAdvice
+public class HttpRequestMethodNotSupportedHandler {
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class) 
+    public ResponseEntity<ApiErrorDTO> httpRequestMethodNotSupportHandler(
+        HttpRequestMethodNotSupportedException exception) {
+            ApiErrorDTO error = new ApiErrorDTO(exception.getMessage());
+            return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(error);
+        }
+}
